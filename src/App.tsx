48c17c4b6c41847af773992e19a493f63aca365b
maruukii <mohamedmagherbi9@gmail.com>
2025-02-18 13:00:12 +0100

    feat: react query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,31 @@
-import React, { useState } from "react";
-import BpmnViewer from "./Pages/BPMNviewer/BpmnViewer";
-import FileUploadButton from "./components/Buttons/fileUploadButton";
-import { RootState } from "./store/store";
-import { useSelector, useDispatch } from "react-redux";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import {
-  clearFileError,
-  clearFileExportSuccess,
-  clearFileImportSucess,
-} from "./store/bpm/fileSlice";
+import React from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import BpmnHome from "./Pages/BPMNhome/bpmnHome";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+// const queryClient = useQueryClient();
+
 const App: React.FC = () => {
-  const [xml, setXml] = useState<string | null>(null);
-  const dispatch = useDispatch();
-  const { filename, fileExportSuccess, fileImportSuccess, fileError } =
-    useSelector((state: RootState) => state.file);
-
-  fileError
-    ? toast.error(fileError) && dispatch(clearFileError())
-    : filename
-    ? fileImportSuccess
-      ? toast.success(filename + " imported successfully!") &&
-        dispatch(clearFileImportSucess())
-      : fileExportSuccess
-      ? toast.success(filename + " exported successfully!") &&
-        dispatch(clearFileExportSuccess())
-      : null
-    : null;
   return (
-    <div>
-      <ToastContainer position="top-center" autoClose={2000} />
-      <FileUploadButton setXml={setXml} />
-
-      {xml ? (
-        <BpmnViewer xml={xml} filename={filename} />
-      ) : (
-        <p style={{ margin: "1rem" }}>waiting for BPMN diagram...</p>
-      )}
-    </div>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" />
+      <BpmnHome />
+    </PersistQueryClientProvider>
   );
 };
 
---
+++ src/Pages/BPMNhome/bpmnHome.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import BpmnViewer from "../../Pages/BPMNviewer/BpmnViewer";
+import FileUploadButton from "../../components/Buttons/fileUploadButton";
+import { RootState } from "../../store/store";
+import { useSelector, useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  clearFileError,
+  clearFileExportSuccess,
+  clearFileImportSucess,
+} from "../../store/bpm/fileSlice";
+const bpmnHome: React.FC = () => {
+  const [xml, setXml] = useState<string | null>(null);
+  const dispatch = useDispatch();
+  const { filename, fileExportSuccess, fileImportSuccess, fileError } =
+    useSelector((state: RootState) => state.file);
+
+  fileError
+    ? toast.error(fileError) && dispatch(clearFileError())
+    : filename
+    ? fileImportSuccess
+      ? toast.success(filename + " imported successfully!") &&
+        dispatch(clearFileImportSucess())
+      : fileExportSuccess
+      ? toast.success(filename + " exported successfully!") &&
+        dispatch(clearFileExportSuccess())
+      : null
+    : null;
+  return (
+    <div>
+      <ToastContainer position="top-center" autoClose={2000} />
+      <FileUploadButton setXml={setXml} />
+
+      {xml ? (
+        <BpmnViewer xml={xml} filename={filename} />
+      ) : (
+        <p style={{ margin: "1rem" }}>waiting for BPMN diagram...</p>
+      )}
+    </div>
+  );
+};
+
+export default bpmnHome;
