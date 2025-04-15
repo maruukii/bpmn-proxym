94fc511c86764c648e148eeb9da4e4bc3b0a8054
maruukii <mohamedmagherbi9@gmail.com>
2025-04-15 16:31:25 +0100

    chore: moved global declarations to the types folder

--- src/global.d.ts
+++ src/global.d.ts
@@ -7,3 +7,23 @@ declare module "*.xml" {
     const content: string;
     export default content;
   }
+  declare module "lodash";
+  declare module "bpmn-moddle";
+
+
+
+  import { MessageApiInjection } from 'naive-ui/lib/message/src/MessageProvider'
+import { ElementLike } from 'diagram-js/lib/core'
+
+declare global {
+  interface Window {
+    bpmnInstances: any
+    __messageBox: MessageApiInjection
+  }
+
+  type BpmnElement = ElementLike & { type: string }
+}
+
+declare interface Window {
+  bpmnInstances: any
+}
